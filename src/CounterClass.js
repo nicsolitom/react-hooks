3d9996fb3bf41c3abfbab7d75b010eab5069ba48
Nic Solitom <nicsolitom@gmail.com>
2020-01-27 11:01:21 +0100

    Added: CounterClass.js, CounterHook.js. Removed logo and implemented our 2 new counters.

---
+++ src/CounterClass.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+export default class CounterClass extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            counter: 0
+        }
+    }
+  render() {
+    return (
+      <div>
+        <h2>ClassCounter</h2>
+        <p>Your counter is: {this.state.counter}</p>
+        <button onClick={() => this.setState(
+            // { counter:this.state.counter +1 }
+            prevState => ({ counter: prevState.counter + 1})
+            )}>Increment +1 </button>
+      </div>
+    );
+}
+}
---
+++ src/CounterHook.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CounterHook() {
+    return (
+        <div>
+            <h2>CounterHook</h2>
+        </div>
+    )
+}
